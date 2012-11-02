5636578d4218a98807e914e878f47633a8e5c315
Greg Whalin <greg@whalin.com>
2012-11-02 18:14:18 -0400

    Initial checkin

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+ruby '1.9.3'
+gem 'rails', '3.2.8'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+	gem 'rspec'
+	gem 'sqlite3'
+end
+
+gem 'bcrypt-ruby', '~> 3.0.0'
+gem 'dalli'
+gem 'decent_exposure'
+gem 'devise'
+gem 'jquery-rails'
+gem 'kaminari'
+gem 'permanent_records'
+gem 'thin'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mentor::Application.config.secret_token = 'f9221d0f0d5731ad6846207b2b5f73c90a677057dd7e69f5c34995ea8bbec45459ad93f2cca770fa3fbf706fb2b31a9cfb7d3de3a83433b8cacab8d50e339885'
